a8bf0eb7fb251c5d5c052fec4aeb96de0ba9b152
joaquinhojman <hojmanjoaquin@gmail.com>
2022-06-18 11:19:31 -0300

    saco unwraps en entidades

--- aerolinea/src/main.rs
+++ aerolinea/src/main.rs
@@ -1,13 +1,18 @@
-use std::io::Read;
-use std::net::{TcpListener, TcpStream, UdpSocket};
+use std::net::UdpSocket;
+use std::process::exit;
 
 fn main() {
     println!("Hello, world!");
-    let sock = UdpSocket::bind("localhost:1236").unwrap();
+    let sock: UdpSocket;
+    if let Ok(udpsock) = UdpSocket::bind("localhost:1236") {
+        sock = udpsock;
+    } else {
+        exit(1);
+    }
     loop {
         let mut buf = [0u8; 16];
         println!("awaiting buffer");
-        let (_, _) = sock.recv_from(&mut buf).unwrap();
+        let _r = sock.recv_from(&mut buf);
         println!("{:?}", buf);
     }
 }
