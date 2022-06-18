57fa7a8484861be09b0cb593c999976e5fd95b70
joaquinhojman <hojmanjoaquin@gmail.com>
2022-06-18 10:09:26 -0300

    aerolinea y banco

--- aerolinea/src/main.rs
+++ aerolinea/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::Read;
+use std::net::{TcpListener, TcpStream, UdpSocket};
+
 fn main() {
     println!("Hello, world!");
+    let sock = UdpSocket::bind("localhost:1236").unwrap();
+    loop {
+        let mut buf = [0u8; 16];
+        println!("awaiting buffer");
+        let (_, _) = sock.recv_from(&mut buf).unwrap();
+        println!("{:?}", buf);
+    }
 }
