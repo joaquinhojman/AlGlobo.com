7660262bfde01c31accdfc5aaa8b2df30f5e00f1
leogm99 <lgiampieri62@gmail.com>
2022-06-17 23:20:02 -0300

    Added common lib, fixed deserialization bug, transactionality in progress

--- alglobo/src/transaction_dispatcher.rs
+++ alglobo/src/transaction_dispatcher.rs
@@ -1,9 +1,8 @@
-use crate::entity_data::Entity;
-use crate::entity_messenger::{EntityMessenger, ServeTransaction};
-use crate::transaction::Transaction;
+use crate::entity_sender::{EntitySender, ServeTransaction};
 use actix::{Actor, Addr, Context, Handler, Message};
+use alglobo_common_utils::transaction_request::TransactionRequest;
+
 use csv::StringRecord;
-use std::collections::HashMap;
 
 const HEADER_ID: &str = "id";
 const HEADER_HOTEL: &str = "hotel_cost";
@@ -11,11 +10,11 @@ const HEADER_BANK: &str = "bank_cost";
 const HEADER_AIRLINE: &str = "airline_cost";
 
 pub struct TransactionDispatcher {
-    messenger: Addr<EntityMessenger>,
+    messenger: Addr<EntitySender>,
 }
 
 impl TransactionDispatcher {
-    pub fn new(messenger: Addr<EntityMessenger>) -> Self {
+    pub fn new(messenger: Addr<EntitySender>) -> Self {
         TransactionDispatcher { messenger }
     }
 }
@@ -35,7 +34,7 @@ impl ReceiveTransaction {
         ReceiveTransaction { transaction }
     }
 
-    pub fn deserialize(&self) -> Transaction {
+    pub fn deserialize(&self) -> TransactionRequest {
         let header = StringRecord::from(vec![HEADER_ID, HEADER_HOTEL, HEADER_BANK, HEADER_AIRLINE]);
         let raw_transaction = self.transaction.clone();
         match raw_transaction.deserialize(Some(&header)) {
@@ -58,6 +57,6 @@ impl Handler<ReceiveTransaction> for TransactionDispatcher {
         let transaction = raw_transaction.deserialize();
         let msg = ServeTransaction::new(transaction);
         println!("[DISPATCHER] sending to messenger");
-        let _ = self.messenger.send(msg);
+        let _ = self.messenger.do_send(msg);
     }
 }
--- hotel/src/main.rs
+++ hotel/src/main.rs
@@ -1,13 +1,22 @@
-use std::io::Read;
-use std::net::{TcpListener, TcpStream, UdpSocket};
+use std::net::UdpSocket;
+use alglobo_common_utils::entity_payload::EntityPayload;
+use rand::{Rng, thread_rng};
+
 
 fn main() {
     println!("Hello, world!");
     let sock = UdpSocket::bind("localhost:1234").unwrap();
+    let mut rng = thread_rng();
     loop {
         let mut buf = [0u8; 16];
         println!("awaiting buffer");
-        let (_, _) = sock.recv_from(&mut buf).unwrap();
-        println!("{:?}", buf);
+        let (_, addr) = sock.recv_from(&mut buf).unwrap();
+        let v = buf.to_vec();
+        let payload_deserialized: EntityPayload = v.into();
+        println!("Received: {:?} from addr {}", payload_deserialized, addr);
+        let x: f64 = rng.gen();
+        println!("{}", x);
+
+        sock.send_to(&buf, addr).unwrap();
     }
 }
